ff9bc4718131299b20567934195986a13d0f9573
Fabio Romita <romitafabio@gmail.com>
2016-12-29 12:23:17 +0100

    after publish

--- packages/collection-fast/package.js
+++ packages/collection-fast/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'hotello:collection-fast',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'Get a collection, fast!',
   // URL to the Git repository containing the source code for this package.
